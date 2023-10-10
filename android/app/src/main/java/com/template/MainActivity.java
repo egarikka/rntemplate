a220354623f0fe926376e7a3955c60e706c094e8
yanovskij_e <yanovsij_e@itstep.org>
2023-10-10 10:23:45 +0300

    splash-config

--- android/app/src/main/java/com/template/MainActivity.java
+++ android/app/src/main/java/com/template/MainActivity.java
@@ -1,9 +1,10 @@
 package com.template;
-
+import android.os.Bundle; 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -23,6 +24,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
+    SplashScreen.show(this); 
     return new DefaultReactActivityDelegate(
         this,
         getMainComponentName(),
